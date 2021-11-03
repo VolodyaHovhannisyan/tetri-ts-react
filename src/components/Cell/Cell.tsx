de1f545324072b02b4a9e766c74b74f3b4e1ae58
Volodya Hovhannisyan <80251069+VolodyaHovhannisyan@users.noreply.github.com>
2021-11-03 16:10:09 +0400

    Add files via upload

---
+++ src/components/Cell/Cell.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { TETROMINOS } from "../../setup";
+import { StyledCell } from "./Cell.styles";
+
+type Props = {
+    type: keyof typeof TETROMINOS;
+}
+
+const Cell: React.FC<Props> = ({ type }) => <StyledCell type={type} color={TETROMINOS[type].color} /> 
+
+export default React.memo(Cell)
---
+++ src/hooks/useStage.ts
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react'
+import { createStage } from '../gameHelpers'
+import type { Player } from './usePlayer'
+//import type { STAGECELL, STAGE } from '../components/Stage/Stage'
+export type STAGECELL = [string | number, string]
+export type STAGE = STAGECELL[][]
+
+export const useStage = (player: Player, resetPlayer: () => void) => {
+    const [stage, setStage] = useState(createStage())
+    const [rowsCleared, setRowsCleared] = useState(0)
+
+    useEffect(() => {
+        if (!player.pos) return;
+
+        setRowsCleared(0)
+
+        const sweepRows = (newStage: STAGE): STAGE => {
+            return newStage.reduce((acc, row) => {
+                if (row.findIndex(cell => cell[0] === 0) === -1) {
+                    setRowsCleared(prev => prev + 1)
+                    acc.unshift(new Array(newStage[0].length).fill([0, 'clear']) as STAGECELL[])
+                    return acc
+                }
+                acc.push(row)
+                return acc
+            }, [] as STAGE)
+        }
+
+        const updateStage = (prevStage: STAGE): STAGE => {
+
+            const newStage = prevStage.map(
+                row => row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell) as STAGECELL)
+            )
+
+            player.tetromino.forEach((row, y) => {
+                row.forEach((value, x) => {
+                    if (value !== 0) {
+                        newStage[y + player.pos.y][x + player.pos.x] = [value, `${player.collided ? 'merged' : 'clear'}`]
+                    }
+                })
+            })
+
+            if (player.collided) {
+                resetPlayer()
+                return sweepRows(newStage)
+            }
+
+            return newStage
+        }
+        setStage(prev => updateStage(prev))
+
+    }, [player.collided, player.pos?.x, player.pos?.y, player.tetromino])
+    
+    return { stage, setStage, rowsCleared}
+}
